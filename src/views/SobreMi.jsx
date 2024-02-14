c6f24176624f1e759941a80aaa04a3a3f0a1dcbe
Maximiliano17 <maxidossantosburgos@gmail.com>
2024-02-13 22:57:37 -0300

    Subir arreglos de res

--- src/views/SobreMi.jsx
+++ src/views/SobreMi.jsx
@@ -18,7 +18,7 @@ function SobreMi() {
        <section className={styles.sobreInfo}> 
          <h2 className={styles.sombreado}>Soy </h2>
          <p>A lo largo de 2022 y 2023, participé en diversas exposiciones y discursos, presentando mis proyectos más significativos. Esta experiencia me ha enseñado la habilidad de vender mis ideas y mejorar mi interacción con personas de diversos perfiles. Desde explicar los beneficios administrativos de un proyecto a una institución hasta transmitir el impacto social de un proyecto a un público más general, he desarrollado una capacidad integral de comunicación.</p>
-       </section> 
+       </section>  
      </div>
     </>
   )
